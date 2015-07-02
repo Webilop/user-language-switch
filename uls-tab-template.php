d96bc547aeef94e2630307d1bfbe67abf6632766
Jefer <yfreldragon@gmail.com>
2015-07-02 08:33:36 -0700

    fixed the url flag in home page

--- uls-tab-template.php
+++ uls-tab-template.php
@@ -197,8 +197,12 @@ switch($position){
 	<?php foreach ($languages as $key => $value) : ?>
 		<div class="tab_flag">
 			<?php $tagHtml = ' <img src="'. plugins_url("css/blank.gif", __FILE__). '" style="margin-right:5px;" class="flag_32x32 flag-'. strtolower(substr($value, -2)) .'" alt="'. $value .'" /> ';
+
+			if (is_home())
+					echo uls_get_link(null, $value, $tagHtml);
+			else
+					echo uls_get_link($postId, $value, $tagHtml);
 ?>
-			<?= uls_get_link($postId, $value, $tagHtml) ?>
 		</div>
 	<?php endforeach; ?> 
 </div>
