4b3eb587c4cc87520e4ac4c6620d993bb133cb7a
Jefer <yfreldragon@gmail.com>
2015-09-02 11:48:14 -0700

    change the style to the tab_position, this styles were in the template now there are in a function and register with wp_enqueue_scripts

--- uls-functions.php
+++ uls-functions.php
@@ -364,6 +364,197 @@ function tap_user_language_switch() {
     include_once('uls-tab-template.php');
 } 
 
+// this function is for create the styles conditions
+add_action( 'wp_enqueue_scripts', 'uls_tab_background_color_picker' );
+function uls_tab_background_color_picker() {
+
+  $options = get_option('uls_settings');
+  $position = $options['tab_position_language_switch']; 
+
+  $TabStyle = "";
+  $TabBackground = "background-color: ".$options['tab_color_picker_language_switch'].";"; 
+  $TabFixed = ($options['fixed_position_language_switch']) ? "position: fixed;" : "position: absolute;";
+  $bodyRelative = ($options['fixed_position_language_switch']) ? "" : "position: relative;";
+  switch($position) {
+    case 'TL':
+      $TabStyle = "#tab_background_color_picker{
+          top: 0;
+          left: 0;
+          width: auto; ".
+          $TabFixed.
+          $TabBackground."
+          padding: 5px 5px 5px 10px;
+          border-radius: 0 0 15px 15px;
+          z-index: 10000000000;
+        }
+      .tab_flag {
+        display: inline;
+      }";
+    break;
+    case 'TC':
+      $TabStyle = "#tab_background_color_picker{
+          top: 0;
+          left: 50%;
+          width: auto;".
+          $TabFixed.
+          $TabBackground."
+          padding: 5px 5px 5px 10px;
+          border-radius: 0 0 15px 15px;
+          z-index: 10000000000;
+        }
+        .tab_flag {
+          display: inline;
+        }";
+    break;
+    case 'TR':
+      $TabStyle = "#tab_background_color_picker{
+          top: 0;
+          right: 0;
+          width: auto;".
+          $TabFixed.
+          $TabBackground."
+          padding: 5px 5px 5px 10px;
+          border-radius: 0 0 15px 15px;
+          z-index: 10000000000;
+        }
+        .tab_flag {
+          display: inline;
+        }";
+    break;
+    case 'BL':
+      $TabStyle = "#tab_background_color_picker{
+        bottom: 0;
+        left: 0;
+        width: auto;".
+        $TabFixed.
+        $TabBackground."
+        padding: 5px 5px 5px 10px;
+        border-radius: 15px 15px 0 0;
+        z-index: 10000000000;
+      }
+      .tab_flag {
+        display: inline;
+      }
+      body {
+        $bodyRelative
+      }"; 
+    break;
+    case 'BC':
+      $TabStyle = "#tab_background_color_picker{
+        bottom: 0;
+        left: 50%;
+        width: auto;".
+        $TabFixed.
+        $TabBackground."
+        padding: 5px 5px 5px 10px;
+        border-radius: 15px 15px 0 0;
+        z-index: 10000000000;
+      }
+      .tab_flag {
+        display: inline;
+      }
+      body {
+        $bodyRelative
+      }";
+    break;
+    case 'BR':
+      $TabStyle = "#tab_background_color_picker{
+        bottom: 0;
+        right: 0;
+        width: auto;".
+        $TabFixed.
+        $TabBackground."
+        padding: 5px 5px 5px 10px;
+        border-radius: 15px 15px 0 0;
+        z-index: 10000000000;
+      }
+      .tab_flag {
+        display: inline;
+      }
+      body {
+        $bodyRelative
+      }";
+    break;
+    case 'LT':
+      $TabStyle = "#tab_background_color_picker{
+        top: 0;
+        left: 0;
+        height: auto;".
+        $TabFixed.
+        $TabBackground."
+        padding: 5px 5px 5px 10px;
+        border-radius: 0 15px 15px 0;
+        z-index: 10000000000;
+      }";
+    break;
+    case 'LM':
+      $TabStyle = "#tab_background_color_picker{
+        top: 50%;
+        left: 0;
+        height: auto;".
+        $TabFixed.
+        $TabBackground."
+        padding: 5px 5px 5px 10px;
+        border-radius: 0 15px 15px 0;
+        z-index: 10000000000;
+      }";
+    break;
+    case 'LB':
+      $TabStyle = "#tab_background_color_picker{
+        bottom: 0;
+        left: 0;
+        height: auto;".
+        $TabFixed.
+        $TabBackground."
+        padding: 5px 5px 5px 10px;
+        border-radius: 0 15px 15px 0;
+        z-index: 10000000000;
+      }";
+    break;
+    case 'RT':
+      $TabStyle = "#tab_background_color_picker{
+        top: 0;
+        right: 0;
+        height: auto;".
+        $TabFixed.
+        $TabBackground."
+        padding: 5px 4px 5px 10px;
+        border-radius: 15px 0 0 15px;
+        z-index: 10000000000;
+      }";
+    break;
+    case 'RM':
+      $TabStyle = "#tab_background_color_picker{
+        top: 50%;
+        right: 0;
+        height: auto;".
+        $TabFixed.
+        $TabBackground."
+        padding: 5px 4px 5px 10px;
+        border-radius: 15px 0 0 15px;
+        z-index: 10000000000;
+      }";
+    break;
+    case 'RB':
+    $TabStyle = "#tab_background_color_picker{
+      bottom: 0;
+      right: 0;
+      height: auto;".
+      $TabFixed.
+      $TabBackground."
+      padding: 5px 4px 5px 10px;
+      border-radius: 15px 0 0 15px;
+      z-index: 10000000000;
+    }";
+    break;
+  }
+?>
+  <style type="text/css"> 
+    <?= $TabStyle; ?>
+  </style> 
+<?php
+}
+
 // this function is for automatic traduction menues
 function uls_traduction_automatic_menu($object)
 {      
